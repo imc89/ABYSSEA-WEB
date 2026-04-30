fe275cda0a267bf7cb128336cf3dfcc7480f9c28
Iñigo Montánchez <mci.m89@gmail.com>
2026-04-30 16:15:07 +0200

    feat: improve card list cursor

--- src/scripts/galleryLoaders/gallery_1Loader.js
+++ src/scripts/galleryLoaders/gallery_1Loader.js
@@ -19,11 +19,11 @@ function table() {
       // Agregamos propiedades flex, wrap, justify-center al contenedor principal.
       // We add flex, wrap, justify-center properties to the main container.
       imageContainer.style.display = 'flex';
-      imageContainer.style.flexWrap = 'wrap'; 
+      imageContainer.style.flexWrap = 'wrap';
       imageContainer.style.justifyContent = 'center';
       // Agregamos la clase glow a cada card.
       // We add the glow class to each card.
-      imageDiv.className = 'glow-img-container'; 
+      imageDiv.className = 'glow-img-container';
       // Creamos un nuevo componente imagen para cada cada card.
       // We create a new image component for each card.
       const imageElement = document.createElement('img');
@@ -33,9 +33,11 @@ function table() {
       // Damos propiedades a la imagen.
       // We give properties to the image.
       imageElement.className = 'glow-img'
+      imageElement.classList.add('cursor-hover');
       imageElement.style.margin = "20px"
       imageElement.style.width = '140px'
       imageElement.classList.add = "glow"
+
       // Agregamos los enlaces de redirección cuando pulsemos cada imagen.
       // We add the redirect links when we click each image.
       let imageURL = `./fishes/fishes.html?id=${i}`;
@@ -53,7 +55,7 @@ function table() {
       });
       // Agregamos la imagen al div contenedor.
       // We add the image to the container div.
-      imageDiv.appendChild(imageElement); 
+      imageDiv.appendChild(imageElement);
       // Agregamos el div contenedor al div general.
       // We add the container div to the general div.
       imageContainer.appendChild(imageDiv);
